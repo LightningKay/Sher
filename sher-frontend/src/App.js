66473848f00426ec4e022e49be77ea70bbf81009
BeefCat29 <32917045+BeefCat29@users.noreply.github.com>
2022-06-25 19:36:07 -0700

    Adding Routes

--- sher-frontend/src/App.js
+++ sher-frontend/src/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+} from 'react-router-dom';
 import './App.css';
+import LoginPage from './components/authPages/LoginPages/LoginPage';
+import RegisterPage from './components/authPages/RegisterPages/RegisterPage';
+import DashBoard from './components/authPages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">HELLO THIS IS APPLICATION</div>
+    <Router>
+        <Routes>
+          <Route path='/login' element={<LoginPage/>}>
+          </Route>
+
+          <Route path='/register' element={<RegisterPage/>}>
+          </Route>
+
+          <Route path='/' element={<DashBoard/>}>
+          </Route>
+        </Routes>
+    </Router>
   );
 }
 
